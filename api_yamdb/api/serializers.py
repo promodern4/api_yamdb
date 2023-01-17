1f9d0350a32dab71819e2a7cb323603d5b1acf53
Павел Полетаев <pasha.poletaev14@mail.ru>
2023-01-17 15:09:14 +0300

    beginning for Title

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from posts.models import Category, Genre, Title
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        slug_field='slug', many=True, queryset=Genre.objects.all()
+    )
+    category = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Category.objects.all()
+    )
+
+    class Meta:
+        model = Title
+        fields = '__all__'
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .permissions import IsAdminOrReadOnly
+from .serializers import TitleSerializer
+from posts.models import Title
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (IsAdminOrReadOnly,)
