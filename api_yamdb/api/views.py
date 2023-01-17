fdce36817fce60cc1f3959e3ad6bd20a91bb4d8b
Павел Полетаев <pasha.poletaev14@mail.ru>
2023-01-17 15:55:37 +0300

    beginning for Category

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .permissions import IsAdminOrReadOnly
+from .serializers import CategorySerializer
+from posts.models import Category
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly,)
--- api_yamdb/posts/models.py
+++ api_yamdb/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Название',
+                            max_length=256)
+    slug = models.SlugField(verbose_name='Указатель',
+                            max_length=50,
+                            unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
